372825ede72fa316c35fdce1b2f35662743f9583
Shashank P <shashankp.0376@gmail.com>
2022-09-22 21:45:48 +0530

    links changed

--- GD/main/views.py
+++ GD/main/views.py
@@ -4,3 +4,11 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home.html')
 
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def projects(request):
+    return render(request, 'projects.html')
