2b1bdc1db5a8ef1ac99c9a0a91e0078e54277fc2
Shashank P <shashankp.0376@gmail.com>
2022-09-24 19:35:40 +0530

    add email handling

--- GD/main/models.py
+++ GD/main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class email(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=15)
+    email = models.EmailField(max_length=15)
+    subject = models.CharField(max_length=15)
+    message = models.TextField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    def __unicode__(self):
+        return 
--- GD/main/views.py
+++ GD/main/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
+from main.models import email
 
 # Create your views here.
 def home(request):
@@ -8,7 +12,18 @@ def about(request):
     return render(request, 'about.html')
 
 def contact(request):
+    if request.method == "POST":
+        fname=request.POST.get('name')
+        femail=request.POST.get('email')
+        fsubject=request.POST.get('subject')
+        fdesc=request.POST.get('message')
+        query=email(name=fname,email=femail,subject=fsubject,message=fdesc)
+        query.save()
+        messages.success(request, "Thanks for contacting us. We will get back to you soon!")
+        return redirect('/contact')
+        
     return render(request, 'contact.html')
+   
 
 def projects(request):
     return render(request, 'projects.html')
